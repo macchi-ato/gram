d6ed0bbc873b05ae05ddf5673f7678a20ff1d60f
macchi-ato <macchikay@gmail.com>
2026-04-13 10:26:16 -0400

    Turn off echoing

--- gram.c
+++ gram.c
@@ -1,8 +1,26 @@
 #include <unistd.h>
+#include <termios.h>
+
+/* 
+    *
+    *   Turn off echoing to enable raw mode
+    * 
+*/
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    raw.c_cflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-    char c;
+    enableRawMode();
 
+    char c;
+    
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
 }
