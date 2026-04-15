13a132141ef0ed9b89baa8a3036527114bec68f2
macchi-ato <macchikay@gmail.com>
2026-04-15 09:27:18 -0400

    error handling

--- gram.c
+++ gram.c
@@ -3,16 +3,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
+
+struct termios orig_termios;
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
 
 /* 
     *
     *   Disable raw mode
     * 
 */
-struct termios orig_termios;
-
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 /* 
@@ -22,7 +30,9 @@ void disableRawMode() {
 */
 void enableRawMode() {
     // Save current terminal settings into orig_termios
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -52,8 +62,9 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
 
 int main() {
@@ -61,7 +72,7 @@ int main() {
 
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
