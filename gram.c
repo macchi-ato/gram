fd695a2c978721de076d10b4a742594ada8d0618
macchi-ato <macchikay@gmail.com>
2026-04-16 10:07:40 -0400

    clear the screen on exit

--- gram.c
+++ gram.c
@@ -28,6 +28,8 @@ struct termios orig_termios;    // Terminal attributes global variable
 */
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -115,6 +117,8 @@ void editorProcessKeypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
