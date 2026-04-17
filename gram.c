802066cf112591d4910536255986a81a2bd36c54
macchi-ato <macchikay@gmail.com>
2026-04-17 14:28:04 -0400

    window size

--- gram.c
+++ gram.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 /* 
     *
@@ -20,6 +21,8 @@
 */
 
 struct editorConfig {
+    int screenrows;
+    int screencols;
     struct termios orig_termios;    // Terminal attributes
 };
 
@@ -95,6 +98,19 @@ char editorReadKey() {
     return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+
+        return 0;
+    }
+}
+
 /* 
     *
     *   Output
@@ -104,7 +120,7 @@ char editorReadKey() {
 void editorDrawRows() {
     int y;
 
-    for (y = 0; y < 24; y++) {
+    for (y = 0; y < E.screenrows; y++) {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
@@ -147,8 +163,13 @@ void editorProcessKeypress() {
     * 
 */
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
