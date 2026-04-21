976dcd2870b7f4396a6c82aebda8890b3532083d
macchi-ato <macchikay@gmail.com>
2026-04-21 09:19:17 -0400

    move cursor

--- gram.c
+++ gram.c
@@ -24,6 +24,7 @@
 */
 
 struct editorConfig {
+    int cx, cy;
     int screenrows;
     int screencols;
     struct termios orig_termios;    // Terminal attributes
@@ -209,7 +210,10 @@ void editorRefreshScreen() {
 
     editorDrawRows(&ab);
 
-    abAppend(&ab, "\x1b[H", 3);
+    char buf[32];
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+    abAppend(&ab, buf, strlen(buf));
+
     // Set more
     abAppend(&ab, "\x1b[?25h", 6);
     
@@ -242,6 +246,9 @@ void editorProcessKeypress() {
 */
 
 void initEditor() {
+    E.cx = 0;
+    E.cy = 0;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
@@ -258,4 +265,4 @@ int main() {
 }
 
 // Where I left of
-// Append buffer
+// Move the cursor
