5cf8051421c1710bc563e1b9ddeec11eddd6116b
macchi-ato <macchikay@gmail.com>
2026-04-27 09:21:02 -0400

    horizontal scroll

--- gram.c
+++ gram.c
@@ -49,6 +49,7 @@ typedef struct erow {
 struct editorConfig {
     int cx, cy;
     int rowoff; // Row offset
+    int coloff; // Column offset
     int screenrows;
     int screencols;
     int numrows;
@@ -277,11 +278,20 @@ void abFree(struct abuf *ab) {
 */
 
 void editorScroll() {
+    // Handle vertical scroll
     if (E.cy < E.rowoff) {
         E.rowoff = E.cy;
     }
     if (E.cy >= E.rowoff + E.screenrows) {
         E.rowoff = E.cy - E.screenrows + 1;
+    }
+
+    // Handle horizontal scroll
+    if (E.cx < E.coloff) {
+        E.coloff = E.cx;
+    }
+    if (E.cx >= E.coloff + E.screencols) {
+        E.coloff = E.cx - E.screencols + 1;
     }
 }
 
@@ -309,9 +319,11 @@ void editorDrawRows(struct abuf *ab) {
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[filerow].size;
+            int len = E.row[filerow].size - E.coloff;
+            if (len < 0) len = 0;
+
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row[filerow].chars, len);
+            abAppend(ab, &E.row[filerow].chars[E.coloff], len);
         }
 
         // K command
@@ -360,9 +372,7 @@ void editorMoveCursor(int key) {
             }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1) {
-                E.cx++;
-            }
+            E.cx++;
             break;
         case ARROW_UP:
             if (E.cy != 0) {
@@ -423,6 +433,7 @@ void initEditor() {
     E.cx = 0;
     E.cy = 0;
     E.rowoff = 0;
+    E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
 
